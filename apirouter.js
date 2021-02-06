2521df4ad87f78bf6d04439745f0dc1a76fadbd0
aezem <you@example.com>
2021-02-06 13:33:32 -0800

    progress

--- apirouter.js
+++ apirouter.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-const dbKey = "365tenders1270@"; 
+const dbKey = "365tenders1270%40"; 
 const dbUser = "owner";
 
 const secretKey = "gsdhr<jrjdjzf<ghshe";
@@ -25,16 +25,14 @@ client.connect(err => {
     client.close();
     return;
     }
-    COLLECTION_USERS= client.db("365tenders").collection("members") ? client.db("365tenders").collection("members") : client.db("365tenders").createCollection("members");
-    COLLECTION_POSTS= client.db("365tenders").collection("posts") ? client.db("365tenders").collection("posts") : client.db("365tenders").createCollection("posts");
+    COLLECTION_USERS= client.db("365tenders").collection("members") ;
+    COLLECTION_POSTS= client.db("365tenders").collection("posts") ;
 });
-
-
 
 const authFilter = (req,res,next)=>{
     let creds;
     try{
-    creds  = jwt.verify(req.header("Cookie"),secretKey)
+    creds = jwt.verify(req.header("Cookie").match(/auth=(\S*?);?$/)[1],secretKey)
     }catch(e){
     console.log(e)
     res.sendStatus(404);
@@ -45,27 +43,39 @@ const authFilter = (req,res,next)=>{
     else res.sendStatus(404);
 };
 
-router.post("/register",(req,res)=>{
-    COLLECTION_USERS.createIndex({
-        username : req.data.username,
-        password : req.data.password,
+router.post("/register",express.urlencoded(),async (req,res)=>{
+    let st = await COLLECTION_USERS.insertOne({
+        username : req.body.username,
+        password : req.body.password,
     })
+    console.log(st);
+    if(st){
+        res.set('Set-Cookie',`auth=${jwt.sign({username:req.body.username},secretKey)};`);
+        res.sendStatus(200);
+    }else{
+        res.sendStatus(404);
+    }
 });
-router.get("/posts",authFilter,(req,res)=>{
-    let posts =COLLECTION_POSTS.find("");
-    res.json(JSON.stringify(posts));
+router.get("/posts",async (req,res)=>{
+    let posts = COLLECTION_POSTS.find("").limit(100);
+    let pa = await posts.toArray();
+    res.send(pa);
+    posts.close();
 });
 router.post("/posts",authFilter,(req,res)=>{
-    COLLECTION_POSTS.createIndex({title:"gdsg",body:"gdsgg"});
+    COLLECTION_POSTS({title:"gdsg",body:"gdsgg"});
     res.json(COLLECTION_POSTS.find(""));
 });
-router.get("/auth",express.urlencoded(),(req,res)=>{
-    let col =  COLLECTION_USERS.find({username:req.body.username,password: req.body.password})
-    if(col){
-        res.set('Set-Cookie',jwt.sign({username:req.body.username},secretKey))
+router.post("/auth",express.urlencoded(),async (req,res)=>{
+    let col =  await COLLECTION_USERS.findOne({username:req.body.username,password: req.body.password},
+        )
+    if(col != undefined ){
+        console.log(col);
+        res.set('Set-Cookie',`auth=${jwt.sign({username:req.body.username},secretKey)};`);
+        res.sendStatus(200);
         return;
     }
-    res.sendStatus(200);
+    res.sendStatus(404)
 });
 router.all("*",(req,res)=>{
     es.sendStatus(404);
